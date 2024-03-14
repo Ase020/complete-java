50a85be06d8edbceb0ed6e0c721cbf1d4b52360e
felixNyalenda <felixasejunior@gmail.com>
2024-03-14 17:07:51 +0300

    objects

---
+++ Objects.java
@@ -0,0 +1,17 @@
+class Calculator{
+    int a;
+
+    public int add(int num1, int num2){
+        return num1 + num2;
+    }
+}
+
+public class Objects {
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+        int result = calculator.add(4,5);
+
+        System.out.println(result);
+    }
+}
+
