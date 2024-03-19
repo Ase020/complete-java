62080d2043d35cb5ca22f2d18895b0c58fce1d53
felixNyalenda <felixasejunior@gmail.com>
2024-03-19 09:29:51 +0300

    multi-level-inheritance

---
+++ Calc3.java
@@ -0,0 +1,5 @@
+public class Calc3 extends Calc2 {
+    public double power(int num1, int num2){
+        return Math.pow(num1, num2);
+    }
+}
--- Inheritance.java
+++ Inheritance.java
@@ -9,5 +9,14 @@ public class Inheritance {
         int subResult2 = calc2.subtract(10, 4); //6
         int multiplyResult2 = calc2.multiply(10, 4); //40
         int divideResult2 = calc2.divide(10, 4); //2
+
+        Calc3 calc3 = new Calc3();
+        int addResult3 = calc3.add(10, 4); //14
+        int subResult3 = calc3.subtract(10, 4); //6
+        int multiplyResult3 = calc3.multiply(10, 4); //40
+        int divideResult3 = calc3.divide(10, 4); //2
+        double powerResult3 = calc3.power(4,2); // 16.0
+
+        System.out.println(powerResult3);
     }
 }
